5f55af880d59dde05fa143da5cd1615099325969
Joao Neves <sevenjp@gmail.com>
2017-09-26 20:42:59 +0200

    Make fred rude again

--- src/Network/Slack/Client.hs
+++ src/Network/Slack/Client.hs
@@ -53,6 +53,16 @@ data Typing = Typing { _tId        :: Integer
 instance ToJSON Typing where
   toJSON (Typing i t c) = object ["id" AT..= i, "type" AT..= t, "channel" AT..= c]
 
+data Reply = Reply { _rId :: Integer
+                   , _rType :: String
+                   , _rChannelId :: ChannelId
+                   , _rText :: String
+                   }
+             deriving (Eq, Show, Generic)
+
+instance ToJSON Reply where
+  toJSON (Reply i t c x) = object ["id" AT..= i, "type" AT..= t, "channel" AT..= c, "text" AT..= x]
+
 sendTyping :: ChannelId -> Integer -> Connection -> IO ()
 sendTyping chan count connection = sendTextData connection $ encode typingMsg
   where typingMsg = Typing { _tId = count, _tType = "typing", _tChannelId = chan }
@@ -63,9 +73,22 @@ incCounter counter = atomically $ do
   writeTVar counter (c + 1)
   return c
 
+sendMessage :: ChannelId -> Integer -> String -> Connection -> IO ()
+sendMessage chan count text connection = sendTextData connection $ encode msg
+  where msg = Reply { _rId = count, _rType = "message", _rChannelId = chan, _rText = text }
+
+getReply :: String -> String
+getReply _ = ":reversed_hand_with_middle_finger_extended:"
+
 reply :: SlackEvent -> TVar Integer -> Connection -> IO ()
 reply Message { _seChannelId = chan, _seText = txt } counter connection =
-  incCounter counter >>= \c -> sendTyping chan c connection >> threadDelay 500000
+  let reply = getReply txt
+  in
+    incCounter counter
+    >>= \c -> sendTyping chan c connection
+              >> threadDelay 500000
+              >> incCounter counter
+              >>= \c -> sendMessage chan c reply connection
 
 handleEvent :: SlackEvent -> TVar Integer -> Connection -> IO ()
 handleEvent event@Message { _seChannelId = chan } counter connection = do
